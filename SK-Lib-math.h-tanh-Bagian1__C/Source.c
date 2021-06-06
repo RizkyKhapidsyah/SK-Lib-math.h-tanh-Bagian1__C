cbc479f65fdcb40330bb69e53fbb229bf5064a4e
Rizky Khapidsyah <45317201+RizkyKhapidsyah@users.noreply.github.com>
2021-06-06 09:55:46 +0700

    Update Source.c

--- SK-Lib-math.h-tanh-Bagian1__C/Source.c
+++ SK-Lib-math.h-tanh-Bagian1__C/Source.c
@@ -1,7 +1,12 @@
 /* tanh example */
 #include <stdio.h>      /* printf */
 #include <math.h>       /* tanh, log */
-#include <conio.h>		/* _getch()	*/
+#include <conio.h>	/* _getch()	*/
+
+/*	Source by CPlusPlus
+	Modified For Learn by Rizky Khapidsyah
+	I.D.E : VS2019
+*/
 
 int main() {
 	double param, result;
